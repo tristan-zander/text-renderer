7a6d1716cf95464cabf1b3bccfbcb77b86befa2e
Tristan Zander <knavetnz@gmail.com>
2021-03-18 22:12:27 -0400

    Added a picture of floppa. Text is actually just botched.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 #[macro_use]
 extern crate glium;
 
-use winit::dpi::LogicalSize;
 use glium::{
-    Display, Surface,
     glutin::{
-        ContextBuilder,
-        window::WindowBuilder,
         event::{Event, WindowEvent},
-        event_loop::{ControlFlow, EventLoop}
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+        ContextBuilder,
     },
+    index::PrimitiveType,
+    texture::{MipmapsOption, RawImage2d, Texture2d, UncompressedFloatFormat},
+    Display, DrawParameters, IndexBuffer, Surface, VertexBuffer,
 };
+use std::borrow::Cow;
+use winit::dpi::LogicalSize;
 
-mod shaders;
 mod text_renderer;
+
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+
+mod implement_vertex {
+    use super::Vertex;
+    implement_vertex!(Vertex, position, tex_coords);
+}
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -23,6 +36,40 @@ fn main() {
         .with_resizable(false);
     let ctx_builder = ContextBuilder::new();
     let display = Display::new(win_builder, ctx_builder, &event_loop).unwrap();
+
+    let mut text_renderer = text_renderer::FontRenderer::new(&display);
+    let program = text_renderer::text_rendering_program(&display);
+
+    let raw_img = get_floppa_texture().unwrap();
+    let floppa_tex = Texture2d::with_format(
+        &display,
+        raw_img,
+        UncompressedFloatFormat::U8U8U8U8,
+        MipmapsOption::NoMipmap,
+    )
+    .unwrap();
+    let tex_verts = vec![
+        Vertex {
+            position: [-1.0, -1.0],
+            tex_coords: [0.0, 0.0],
+        },
+        Vertex {
+            position: [-1.0, 1.0],
+            tex_coords: [0.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, 1.0],
+            tex_coords: [1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, -1.0],
+            tex_coords: [1.0, 0.0],
+        },
+    ];
+
+    let floppa_vbo = VertexBuffer::new(&display, &tex_verts).unwrap();
+    let floppa_ibo =
+        IndexBuffer::new(&display, PrimitiveType::TriangleStrip, &[1 as u16, 2, 0, 3]).unwrap();
 
     event_loop.run(move |event, _window_target, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -34,11 +81,59 @@ fn main() {
             } => *control_flow = ControlFlow::Exit,
             _ => {}
         }
-        
         let mut frame = display.draw();
         frame.clear_color(0.4, 0.7, 0.8, 1.0);
 
+        let floppa_uniforms = uniform! {
+            tex: floppa_tex.sampled(),
+        };
+        // Draw debug floppa texture
+        frame.draw(
+            &floppa_vbo,
+            &floppa_ibo,
+            &program,
+            &floppa_uniforms,
+            &DrawParameters {
+                blend: glium::Blend::alpha_blending(),
+                ..Default::default()
+            },
+        ).unwrap();
+
+        // text_renderer.debug_texture(&mut frame, &display, &program);
+
+        text_renderer
+            .render_text("text: &'a str", (0.5, 0.5), &mut frame, &display, &program)
+            .unwrap();
         // frame.draw(vertex_buffer: V, index_buffer: I, program: &Program, uniforms: &U, draw_parameters: &DrawParameters<'_>)
+
         frame.finish().unwrap();
     });
+}
+
+fn get_floppa_texture<'a>() -> Result<RawImage2d<'a, u8>, Box<dyn std::error::Error>> {
+    use image::io::Reader;
+    use std::io::Cursor;
+
+    let floppa_bytes = include_bytes!("../res/floppa.jpg");
+    let reader = Reader::new(Cursor::new(floppa_bytes))
+        .with_guessed_format()?
+        .decode()?.flipv();
+    let image = reader.to_rgba8();
+    let (h, w) = (image.height(), image.width());
+    let raw = image.into_raw();
+
+    let tex = RawImage2d::from_raw_rgba(raw, (h, w));
+
+    Ok(tex)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn image_does_decode() -> Result<(), Box<dyn std::error::Error>> {
+        get_floppa_texture()?;
+        Ok(())
+    }
 }
--- src/text_renderer.rs
+++ src/text_renderer.rs
@@ -1,34 +1,49 @@
+
+use glium::backend::Facade;
 use glium::{
-    backend::Facade,
-    texture::{MipmapsOption, RawImage2d, Texture2d, UncompressedFloatFormat, ClientFormat},
+    texture::{ClientFormat, MipmapsOption, RawImage2d, Texture2d, UncompressedFloatFormat},
+    Program, Surface, Frame, VertexBuffer, IndexBuffer, 
+    index::PrimitiveType
 };
-use rusttype::{
-    gpu_cache::{Cache, CacheBuilder},
-    Font,
-};
+use rusttype::{gpu_cache::*, *};
 use std::borrow::Cow;
 
-struct TextRenderer<'a> {
-    font: Font<'a>,
-    cache: Cache<'a>,
-    tex: Texture2d,
+pub struct RenderedText {
+    texture: Texture2d,
+    size: (u32, u32),
 }
 
-impl<'a> TextRenderer<'a> {
-    fn new<F: Facade>(display: F) -> Self {
-        const SIZE: u32 = 4 * 1024;
+pub struct FontRenderer<'a> {
+    font: Font<'a>,
+    cache: Cache<'a>,
+    cache_tex: Texture2d,
+}
 
-        let font = Font::try_from_bytes(include_bytes!("../res/LinLibertine_R.ttf")).unwrap();
-        let cache = CacheBuilder::default()
-            .dimensions(SIZE, SIZE)
-            .multithread(true)
-            .build();
-        let tex = Texture2d::with_format(
-            &display,
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+mod implement_vertex {
+    use super::Vertex;
+    implement_vertex!(Vertex, position, tex_coords);
+}
+
+impl FontRenderer<'_> {
+    pub fn new<T>(display: &T) -> Self
+    where
+        T: Facade,
+    {
+        let font = include_bytes!("../res/LinLibertine_R.ttf");
+        let font = Font::try_from_bytes(font).unwrap();
+        let cache = gpu_cache::Cache::builder().dimensions(1024, 1024).build();
+
+        let cache_tex = Texture2d::with_format(
+            display,
             RawImage2d {
-                data: Cow::Owned(vec![128u8; (SIZE * SIZE) as usize]),
-                width: SIZE,
-                height: SIZE,
+                data: Cow::Owned(vec![128u8; 1024 * 1024]),
+                width: 1024,
+                height: 1024,
                 format: ClientFormat::U8,
             },
             UncompressedFloatFormat::U8,
@@ -36,6 +51,204 @@ impl<'a> TextRenderer<'a> {
         )
         .unwrap();
 
-        TextRenderer { font, cache, tex }
+        FontRenderer {
+            font,
+            cache,
+            cache_tex,
+        }
+    }
+
+    pub fn get_glyph<T>(&self, id: T) -> Glyph<'_>
+    where
+        T: IntoGlyphId,
+    {
+        self.font.glyph(id)
+    }
+
+    /// Renders text to the specified point on the screen. Only use positions from -1 to 1
+    pub fn render_text<'a, T>(
+        &mut self,
+        text: &'a str,
+        pos: (f32, f32),
+        frame: &mut Frame,
+        display: &T,
+        program: &Program,
+    ) -> Result<(), ()>
+    where
+        T: Facade,
+    {
+        // TODO add scale to arguments or check from Winit display
+        let scale = Scale { x: 100.0, y: 100.0 };
+        let glyphs: Vec<_> = self
+            .font
+            .layout(text, scale, Point { x: pos.0, y: pos.1 })
+            .collect();
+
+
+        // SAFETY: rusttype crate expects the text to exist as long as the cache whenever it is passed into
+        // cache.queue_glyph(). This is irrelevant here because the glyph is getting cached as soon as it
+        // is added later in the code. This code is necessary to evade certain lifetime requirements.
+        unsafe {
+            for g in &glyphs {
+                (*(&self.cache as *const Cache<'_> as *mut Cache<'_>)).queue_glyph(0, g.clone());
+            }
+        }
+
+        {
+            let cache_tex = &self.cache_tex;
+            let cache = &mut self.cache;
+            cache
+                .cache_queued(|rect, data| {
+                    // Not sure why I have to get the main mipmap first
+                    // since the texture doesn't have a mipmap.
+                    // Maybe writing to the texture directly would cause errors?
+                    // It's probably just an API thing.
+                    cache_tex.main_level().write(
+                        glium::Rect {
+                            left: rect.min.x,
+                            bottom: rect.min.y,
+                            width: rect.width(),
+                            height: rect.height(),
+                        },
+                        RawImage2d {
+                            data: Cow::Borrowed(data),
+                            width: rect.width(),
+                            height: rect.height(),
+                            format: ClientFormat::U8,
+                        },
+                    );
+                })
+                .unwrap();
+                cache.clear_queue();
+        }
+
+        let (scr_width, scr_height) = {
+            let (w, h) = frame.get_dimensions();
+            (w as f32, h as f32)
+        };
+
+        let vertices: Vec<_> = glyphs
+            .iter()
+            .filter_map(|g| self.cache.rect_for(0, g).ok().flatten())
+            .flat_map(|(uv_rect, screen_rect)| {
+                let gl_rect = Rect {
+                    min: Point {
+                        x: screen_rect.min.x as f32 / scr_width,
+                        y: screen_rect.min.y as f32 / scr_height,
+                    },
+                    max: Point {
+                        x: screen_rect.max.x as f32 / scr_width,
+                        y: screen_rect.max.y as f32 / scr_height,
+                    },
+                };
+
+                // This can be brought down to 4 vertices
+                // if I add an index buffer
+                vec![
+                    Vertex {
+                        position: [gl_rect.min.x, gl_rect.max.y],
+                        tex_coords: [uv_rect.min.x, uv_rect.max.y],
+                    },
+                    Vertex {
+                        position: [gl_rect.min.x, gl_rect.min.y],
+                        tex_coords: [uv_rect.min.x, uv_rect.min.y],
+                    },
+                    Vertex {
+                        position: [gl_rect.max.x, gl_rect.min.y],
+                        tex_coords: [uv_rect.max.x, uv_rect.min.y],
+                    },
+                    Vertex {
+                        position: [gl_rect.max.x, gl_rect.min.y],
+                        tex_coords: [uv_rect.max.x, uv_rect.min.y],
+                    },
+                    Vertex {
+                        position: [gl_rect.max.x, gl_rect.max.y],
+                        tex_coords: [uv_rect.max.x, uv_rect.max.y],
+                    },
+                    Vertex {
+                        position: [gl_rect.min.x, gl_rect.max.y],
+                        tex_coords: [uv_rect.min.x, uv_rect.max.y],
+                    },
+                ]
+            })
+            .collect();
+
+        let vert_buffer = glium::VertexBuffer::new(display, &vertices).unwrap();
+        let uniform = uniform! {
+            tex: self.cache_tex.sampled()
+        };
+
+
+        frame
+            .draw(
+                &vert_buffer,
+                glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+                program,
+                &uniform,
+                &glium::DrawParameters {
+                    blend: glium::Blend::alpha_blending(),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+        Ok(())
+    }
+
+    pub fn debug_texture<T: Facade>(&mut self, frame: &mut Frame, facade: &T, program: &Program) {
+
+        let data = [
+            // Top left
+            Vertex { position: [-1.0, 1.0], tex_coords: [0.0, 1.0] },
+            // Bottom left
+            Vertex { position: [-1.0, -1.0], tex_coords: [0.0, 0.0] },
+            // Top right
+            Vertex { position: [1.0, 1.0], tex_coords: [1.0, 1.0] },
+            // Bottom right
+            Vertex { position: [1.0, -1.0], tex_coords: [1.0, 0.0] },
+        ];
+        let vbo = VertexBuffer::new(facade, &data).unwrap();
+        let ibo = IndexBuffer::new(facade, PrimitiveType::TrianglesList, &[0 as u16, 2, 1, 3]).unwrap();
+
+        let uniforms = uniform! {
+            tex: self.cache_tex.sampled()
+        };
+
+        frame.draw(&vbo, &ibo, program, &uniforms, &glium::DrawParameters::default()).unwrap();
     }
 }
+
+
+
+pub fn text_rendering_program<T: Facade>(facade: &T) -> glium::Program {
+    glium::Program::from_source(facade, VERT_SHADER, FRAG_SHADER, None).unwrap()
+}
+
+const VERT_SHADER: &str = r"
+#version 330 core
+
+in vec2 position;
+in vec2 tex_coords;
+
+out vec2 TexCoords;
+
+void main()
+{
+    gl_Position = vec4(position.xy, 0.0, 1.0);
+    TexCoords = tex_coords;
+}
+";
+
+const FRAG_SHADER: &str = r"
+#version 330 core
+
+in vec2 TexCoords;
+out vec4 color;
+
+uniform sampler2D tex;
+
+void main()
+{
+    color = texture(tex, TexCoords);
+}
+";
